0803faca84f7dd97ea7132ec57edb74fcf97bb0f
dosbenjamin <dosbenjamin@gmail.com>
2021-06-29 21:44:51 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+yay xorg-xinit
+yay i3-gaps
+yay i3-blocks
+yay rofi
+yay nnn
+
+yay brave-browser
+yay spotify
+yay discord
+yay kitty
+yay visual-studio-code
+yay 1password
+yay caprine
+yay nvim
+yay vlc
