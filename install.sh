0435a3e2feea1ba7fc520c085f02fb5b03ed493a
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-07-11 23:13:30 +0200

    add git config

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 sudo apt update
 sudo apt full-upgrade
 
+# Development environment
+mkdir ~/McArnolds
+mkdir ~/Workspace
+ln -s .gitconfig ~/.gitconfig
+ln -s .gitconfig-mcarnolds ~/.gitconfig-mcarnolds
+ln -s .gitconfig-workspace ~/.gitconfig-workspace
+ln -s .ssh-config ~/.ssh/config
+
 # Fish
 sudo apt install fish && fish
 curl -L https://get.oh-my.fish | fish
-ln -s ./config.fish ~/.config/fish/config.fish
+ln -s config.fish ~/.config/fish/config.fish
 omf install pure
 
 # PHP
