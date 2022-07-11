efe7905760e6a768aa9b096974efd915065e64a6
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-07-11 22:40:22 +0200

    add composer to shell variables

--- config.fish
+++ config.fish
@@ -1,11 +1,12 @@
 # Custom aliases
-alias c="clear"
-alias fishconfig="code ~/.config/fish/config.fish"
-alias fishreload="source ~/.config/fish/config.fish"
-alias ws="cd ~/Workspace"
-alias mca="cd ~/McArnolds"
-alias npm="pnpm"
+alias c='clear'
+alias fishconfig='code ~/.config/fish/config.fish'
+alias fishreload='source ~/.config/fish/config.fish'
+alias ws='cd ~/Workspace'
+alias mca='cd ~/McArnolds'
+alias npm='pnpm'
 
 # Shell variables
 set PATH $HOME/.local/share/pnpm $PATH
+set PATH $HOME/.config/composer/vendor/bin $PATH
 set LS_COLORS 'ow=90;102'
