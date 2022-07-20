955c0d4eb0ae05644ba0b82530fc2b247257a4da
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-07-20 23:00:48 +0200

    fix ssh and git config

--- install.sh
+++ install.sh
@@ -5,16 +5,8 @@ sudo apt full-upgrade
 # Development environment
 mkdir ~/McArnolds
 mkdir ~/Workspace
-ln -s gitconfig ~/.gitconfig
-ln -s gitconfig-mcarnolds ~/.gitconfig-mcarnolds
-ln -s gitconfig-workspace ~/.gitconfig-workspace
-ln -s ssh-config ~/.ssh/config
-
-# Fish
-sudo apt install fish && fish
-curl -L https://get.oh-my.fish | fish
-ln -s config.fish ~/.config/fish/config.fish
-omf install pure
+cp gitconfig ~/.gitconfig
+cp ssh-config ~/.ssh/config
 
 # PHP
 sudo apt install php7.4
@@ -23,3 +15,9 @@ sudo apt install composer
 # Node
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 pnpm env use --global lts
+
+# PostgreSQL
+sudo apt install postgresql
+
+# Fish
+sudo apt install fish && fish
