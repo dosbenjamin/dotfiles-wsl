363fbc78bdd72733982eac61a85784226c0388d9
Benjamin Dos Santos <hello@benjamindossantos.be>
2023-02-25 18:25:12 +0100

    feat: install gitkraken

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,8 @@
 curl -L https://get.oh-my.fish | fish
 cp config.fish ~/.config/fish/config.fish
 omf install pure
+
+# GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo apt install ./gitkraken-amd64.deb
+rm ./gitkraken-amd64.deb
